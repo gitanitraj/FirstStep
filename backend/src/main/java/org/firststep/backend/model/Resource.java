255f8f2b9f787119cd3537faf88ca1d350c8bc82
gitanitraj <anitra.com@gmail.com>
2026-06-03 11:56:27 -0400

    Added resource code

--- backend/src/main/java/org/firststep/backend/model/Resource.java
+++ backend/src/main/java/org/firststep/backend/model/Resource.java
@@ -1,5 +1,56 @@
 package org.firststep.backend.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Resource {
-    
+  public String id;
+  public String category;
+  public String subcategory;
+  public String organization;
+  public String parent_organization;
+  public String summary;
+  public String description;
+  public String population;
+  public String eligibility;
+  public Integer eligibility_age_min;
+  public Integer eligibility_age_max;
+  public String eligibility_gender;
+  public List<Location> locations;
+  public List<Phone> phones;
+  public List<Website> websites;
+  public String county;
+  public List<String> access_mode;
+  public String cost;
+  public String urgency;
+  public List<String> tags;
+  public String source;
+  public String retrieved;
+  public Boolean verified;
+  public String notes;
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Location {
+    public String label;
+    public String address;
+    public String city;
+    public String state;
+    public String zip;
+    public Boolean confidential;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Phone {
+    public String number;
+    public String label;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Website {
+    public String url;
+    public String label;
+  }
 }
+
