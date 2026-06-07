8a52b416b2881a7c8279a427b64978ab884ddfbd
gitanitraj <anitra.com@gmail.com>
2026-06-07 14:19:01 -0400

    Updated app.js file category tags.

--- backend/src/main/java/org/firststep/backend/model/NewsItem.java
+++ backend/src/main/java/org/firststep/backend/model/NewsItem.java
@@ -1,20 +1,27 @@
 package org.firststep.backend.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NewsItem {
 
     public String id;
+    public String type;
     public String headline;
     public String summary;
+    public String body;
+    public String urgency;
+    public String published;
+    public String expires;
+    public String geography;
+    public Boolean verified;
+    public Boolean active;
+    public String author;
 
     @JsonProperty("why_it_matters")
     public String whyItMatters;
-
-    public String urgency;
-    public String published;
 
     @JsonProperty("source_name")
     public String sourceName;
@@ -22,5 +29,9 @@ public class NewsItem {
     @JsonProperty("source_url")
     public String sourceUrl;
 
-    public Boolean active;
+    @JsonProperty("category_tags")
+    public List<String> categoryTags;
+
+    @JsonProperty("resource_tags")
+    public List<String> resourceTags;
 }
